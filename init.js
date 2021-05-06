467dd7e8d4b9592a76e541acaeb7735d514d8ca9
laosier <laosier@qq.com>
2021-05-06 14:59:34 +0900

    make by project

--- init.js
+++ init.js
@@ -46,18 +46,19 @@ async function main() {
     const desc = program.input || 'desc.json';
     console.log('desc : ', desc);
     const content = await readFile(desc);
-    await mkdir('./build/jscallc/');
-    await mkdir('./build/c/');
-    await mkdir('./build/jscallpython/');
-    await mkdir('./build/python/');
-    await mkdir('./build/releasejscallc/');
-    await mkdir('./build/releasejscallpython/');
     const description = JSON.parse(content);
     const {
         class_name,
         init_params,
         funcs,
     } = description;
+    
+    await mkdir('./build/jscallc/' + description.class_name+'/');
+    await mkdir('./build/c/'+description.class_name+'/');
+    await mkdir('./build/jscallpython/' + description.class_name+'/');
+    await mkdir('./build/python/' + description.class_name+'/');
+    await mkdir('./build/releasejscallc/' + description.class_name+'/');
+    await mkdir('./build/releasejscallpython/' + description.class_name+'/');
 
     try {
         const js_c_code = generate_js(class_name, init_params, funcs, desc, version, 'c');
@@ -70,14 +71,14 @@ async function main() {
         } = generate_c(class_name, init_params, funcs, desc, version);
         const python_code = generate_python(class_name, init_params, funcs, desc, version);
         const makefile_code = generate_makefile(class_name);
-        await writeFile(`./build/jscallc/${class_name}_proxy.js`, js_c_code);
-        await writeFile(`./build/c/${class_name}_imp.cpp`, c_imp_code);
-        await writeFile(`./build/c/${class_name}_init.cpp`, c_init_code);
-        await writeFile(`./build/c/${class_name}_header.h`, c_header_code);
-        await writeFile(`./build/c/${class_name}_pipe.cpp`, c_pipe_code);
-        await writeFile(`./build/jscallpython/${class_name}_proxy.js`, js_python_code);
-        await writeFile(`./build/python/${class_name}_imp.py`, python_code);
-        await writeFile(`./build/c/makefile`, makefile_code);
+        await writeFile(`./build/jscallc/${class_name}/${class_name}_proxy.js`, js_c_code);
+        await writeFile(`./build/c/${class_name}/${class_name}_imp.cpp`, c_imp_code);
+        await writeFile(`./build/c/${class_name}/${class_name}_init.cpp`, c_init_code);
+        await writeFile(`./build/c/${class_name}/${class_name}_header.h`, c_header_code);
+        await writeFile(`./build/c/${class_name}/${class_name}_pipe.cpp`, c_pipe_code);
+        await writeFile(`./build/jscallpython/${class_name}/${class_name}_proxy.js`, js_python_code);
+        await writeFile(`./build/python/${class_name}/${class_name}_imp.py`, python_code);
+        await writeFile(`./build/c/${class_name}/makefile`, makefile_code);
     } catch (e) {
         console.trace(e);
     }
