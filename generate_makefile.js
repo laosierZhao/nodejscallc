fc727ad5f06436acb8d4cdafc1081471c939c476
laosier <laosier@qq.com>
2021-05-07 09:32:12 +0900

    make by project

--- generate_makefile.js
+++ generate_makefile.js
@@ -2,7 +2,7 @@ function generate_makefile(func_name) {
 
 return `DIR_INC = -I./
 DIR_LIB = -L./
-TARGET	= ../releasejscallc/${func_name}
+TARGET	= ../../releasejscallc/${func_name}/${func_name}
 CFLAGS = -g -Wall $(DIR_INC) $(DIR_LIB)
 LDFLAGS += -lstdc++
 SRCDIR:=
@@ -10,18 +10,19 @@ SRCS := $(wildcard *.cpp) $(wildcard $(addsuffix /*.cpp, $(SRCDIR)))
 OBJECTS := $(patsubst %.c,%.o,$(SRCS))
 $(TARGET) : $(OBJECTS)
 \tgcc $(CFLAGS) $^ -o $@ $(LDFLAGS)
-\tcp ../jscallc/${func_name}_proxy.js ../releasejscallc/
-\tcp ../jscallpython/${func_name}_proxy.js ../releasejscallpython/
-\tcp ../python/${func_name}_imp.py ../releasejscallpython/
-\tchmod +x ../releasejscallpython/${func_name}_imp.py
+\tcp ../../jscallc/${func_name}/${func_name}_proxy.js ../../releasejscallc/${func_name}/
+\tcp ../../jscallpython/${func_name}/${func_name}_proxy.js ../../releasejscallpython/${func_name}/
+\tcp ../../python/${func_name}/${func_name}_imp.py ../../releasejscallpython/${func_name}/
+\tchmod +x ../../releasejscallpython/${func_name}/${func_name}_imp.py
 %.o : %.c
 \tgcc -c $(CFLAGS) $< -o $@
 clean:
 \t@rm -f *.o $(TARGET)
-\trm -rf ../releasejscallc/*
-\trm -rf ../releasejscallpython/*
+\trm -rf ../../releasejscallc/${func_name}/*
+\trm -rf ../../releasejscallpython/${func_name}/*
 .PHONY:clean`
 
 }
 
 module.exports = generate_makefile;
+
